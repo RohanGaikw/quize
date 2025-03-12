e503d8825672a244b6e54ccb0dc77d3d792919b0
RohanGaikw <gaikwadroyal2002@gmail.com>
2025-03-12 12:59:11 +0530

    new qiuze

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 5000;
+
+const PORT = process.env.PORT || 5000;  // Vercel वर डायनॅमिक PORT वापरतो
 
 app.use(express.json());
 app.use(cors());
@@ -44,3 +45,5 @@ app.post('/api/submit', (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
